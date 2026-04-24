9f41095858f18c57a75d34bc0f5462ffad85f525
Harsh Mishra <harshmishra8011@gmail.com>
2026-04-24 07:47:57 +0530

    fixed some issues

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,36 @@
+import { readFile, readFileSync } from 'fs';
+import path from 'path';
 import * as vscode from 'vscode';
+const getNonce = () => crypto.randomUUID().toString();
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "run-your-sqlite" is now active!');
-	const disposable = vscode.commands.registerCommand('run-your-sqlite.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from Run Your Sqlite!');
+	const basePath = vscode.Uri.joinPath(context.extensionUri, "src", "sqlite-ui", "dist");
+	const htmlPath = vscode.Uri.joinPath(basePath, "index.html");
+	console.log(basePath.toString());
+	const nonce = getNonce();
+	let html = readFileSync(htmlPath.fsPath, "utf-8");
+	const disposable = vscode.commands.registerCommand('run-your-sqlite.openUI', () => {
+		const panel = vscode.window.createWebviewPanel(
+			'runYourSqlite',
+			'Run Your SQLite',
+			vscode.ViewColumn.One,
+			{
+				enableScripts: true,
+				localResourceRoots: [vscode.Uri.joinPath(context.extensionUri, 'sqlite-ui/dist')]
+			}
+		);
+		const webview = panel.webview;
+		html = html.replace(/(src|href)="([^"]+)"/g, (_, attr, path) => {
+			if (path.startsWith('http') || path.startsWith('data:')) {
+				return `${attr}="${path}"`;
+			}
+
+			const fileUri = vscode.Uri.joinPath(basePath, path);
+			const webviewUri = webview.asWebviewUri(fileUri);
+
+			return `${attr}="${webviewUri}"`;
+		});
+		webview.html = html;
 	});
 
 	context.subscriptions.push(disposable);
