e648ace6354e4348395a10a81f310b8c76af2fd0
Harsh Mishra <harshmishra8011@gmail.com>
2026-04-25 00:16:21 +0530

    added react web ui

---
+++ web/src/App.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import './App.css';
+function App() {
+    const [count, setCount] = useState(0);
+    return (<>
+      <p>Hello World</p>
+    </>);
+}
+export default App;
+//# sourceMappingURL=App.js.map
